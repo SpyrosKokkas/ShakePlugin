e225d0336eca16a704d866a1c853bda0053661f5
SpyrosKokkas <56438241+SpyrosKokkas@users.noreply.github.com>
2025-11-22 12:47:32 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
+}
+
+group = "com.s3nko"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+tasks.processResources {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+sourceSets {
+    main {
+        resources.srcDir("src/main/resources")
+    }
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        create("IC", "2025.1")
+        bundledPlugins("org.jetbrains.kotlin")
+        plugin("org.jetbrains.android:243.21565.23")
+
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "251"
+        }
+
+        changeNotes = """
+      Initial version
+    """.trimIndent()
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
---
+++ src/main/kotlin/com/s3nko/shake/ShakeEmulator.kt
@@ -0,0 +1,115 @@
+package com.s3nko.shake
+
+import com.android.ddmlib.IDevice
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import org.jetbrains.android.sdk.AndroidSdkUtils
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class ShakeEmulator : AnAction() {
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project ?: return
+
+        val devices = AndroidSdkUtils.getDebugBridge(project)?.devices
+
+        if (devices.isNullOrEmpty()) {
+            project.showNotification("No Devices Connected", NotificationType.WARNING)
+        } else {
+            devices.forEach { device ->
+                if (device.serialNumber.startsWith("emulator-")) {
+                    executeShakeCommand(project, device)
+                } else {
+                    project.showNotification(
+                        "Device ${device.name}: Cannot simulate shake on physical device",
+                        NotificationType.WARNING
+                    )
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Executes the adb command directly to the emulator
+     * @param project It defines the project instance
+     * @param device It holds the device id so the command knows to which device to send the command
+     *
+     * First we get the adb path of the android sdk from the user configurations,
+     * then, if the path is null, we show a notification so the user can go and configure the Android SDK
+     * to the Android Studio.
+     * If the adbPath is nonNull, we continue with the chained execution of the commands,
+     * that replicate the shake with the emulator virtual sensors.
+     *
+     * If any of the commands fail, it shows a notification with the error message
+     *
+     * In case where there is an adbPath but for some reason if wrong/broken,
+     * it displays an error notification
+     */
+    private fun executeShakeCommand(project: Project, device: IDevice) {
+        val adbPath = getAdbPath(project)
+        if (adbPath == null) {
+            project.showNotification("Android SDK not configured. Cannot find 'adb' executable.", NotificationType.ERROR)
+            return
+        }
+        val serial = device.serialNumber
+
+        val firstCommand = arrayOf(
+            adbPath, "-s", serial, "emu", "sensor", "set", "acceleration", "100:100:100"
+        )
+        val secondCommand = arrayOf(
+            adbPath, "-s", serial, "emu", "sensor", "set", "acceleration", "0:0:0"
+        )
+
+        runCatching {
+            val startProcess = Runtime.getRuntime().exec(firstCommand)
+            val exitCodeFirst = startProcess.waitFor()
+
+            val endProcess = Runtime.getRuntime().exec(secondCommand)
+            val exitCodeSecond = endProcess.waitFor()
+
+            if (exitCodeFirst == 0 && exitCodeSecond == 0) {
+                /**
+                 * Leaving this here for debugging if necessary later
+                 */
+//               project.showNotification(
+//                  "Device ${device.name}: Shake (Sensor Manipulation) sent successfully.",
+//                   NotificationType.INFORMATION
+//                )
+            } else {
+                val errorOutput = BufferedReader(InputStreamReader(endProcess.errorStream)).readText()
+                project.showNotification(
+                    "Device ${device.name}: ADB Emu Command Failed. Output: $errorOutput",
+                    NotificationType.ERROR
+                )
+            }
+        }.onFailure { e ->
+            project.showNotification(
+                "Device ${device.name}: Error executing host command. Check if ADB is in PATH. Message: ${e.message}",
+                NotificationType.ERROR)
+        }
+    }
+
+    /**
+     * Notification extension funtion
+     * @param message It holds the message we want to display
+     * @param type It defines the notification type to display (e.g., Type.INFORMATION)
+     */
+    private fun Project.showNotification(message: String, type: NotificationType) {
+        NotificationGroupManager.getInstance()
+            .getNotificationGroup("ShakeEmulator.Notifications")
+            .createNotification("Shake result", message, type)
+            .notify(this)
+    }
+
+    /**
+     * Retrieves the adb path using the AndroidSdkUtils
+     */
+    fun getAdbPath(project : Project): String? {
+        return AndroidSdkUtils.findAdb(project).adbPath.toString()
+    }
+}
